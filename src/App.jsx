e83da16c979c5c6755e21d8308c202e034e585b9
thanhPhat <thanhphat130113@gmail.com>
2025-06-16 13:19:56 +0700

    video 27

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
+
+  // function myFunc() {
+  //   console.log("runn")
+  // }
+  const myFunc = () => {
+    console.log("runnn")
+  }
+  myFunc();
 
   return (
     <>
